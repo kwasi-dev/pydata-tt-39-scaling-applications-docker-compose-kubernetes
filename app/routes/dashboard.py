2ccfe22e584acece04ea2769c33a5892c18c46c3
kwasiedwards <kwasiedwards@gmail.com>
2025-08-18 22:38:39 +0000

    added dashboard

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -4,16 +4,21 @@ from fastapi import Request, Form, Depends, status
 from app.utilities import get_flashed_messages, oauth2_scheme
 from app.database import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
-from sqlmodel import select
-from app.models import User
+from sqlmodel import select, desc
+from app.models import User, TodoItem
 from app.utilities import flash, verify_password, get_flashed_messages, get_current_user
 from fastapi.security import OAuth2PasswordBearer
 from typing import Annotated
 
 
 @root_router.get("/dashboard", response_class=HTMLResponse)
-async def dashboard_view(request: Request, user: Annotated[str, Depends(get_current_user)]):
+async def dashboard_view(request: Request, user: Annotated[str, Depends(get_current_user)], session: AsyncSession = Depends(get_session),):
+    user_todos = (await session.exec(
+        select(TodoItem).where(TodoItem.user_id == user.id).order_by(desc(TodoItem.id))
+    )).all()
+
+
     return templates.TemplateResponse(
         "dashboard.html",
-        {"request": request, "messages": get_flashed_messages(request), "user": user}
+        {"request": request, "messages": get_flashed_messages(request), "user": user, "todo_items": user_todos}
     )
