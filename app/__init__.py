7e4a5015e6a553bb7d5e5fcad6de73d046fe431a
kwasiedwards <kwasiedwards@gmail.com>
2025-08-18 17:35:12 +0000

    added basic login screen from BS5 examples

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
-from .routes import root_router
+from .routes import root_router, STATIC_PATH
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory=STATIC_PATH), name="static")
 app.include_router(root_router)
